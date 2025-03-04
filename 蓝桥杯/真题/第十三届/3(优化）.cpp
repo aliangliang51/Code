66f8c616b56e99e4028c3c90b0b83aee8cf6cdcb
aliangliang51 <2931769856@qq.com>
2025-03-04 08:30:10 +0800

    修改小明做题的代码

--- 蓝桥杯/真题/第十三届/3(优化）.cpp
+++ 蓝桥杯/真题/第十三届/3(优化）.cpp
@@ -6,17 +6,18 @@ int main() {
 	long long count=0;
 	
 	per = a*5+b*2;//每天 
-	count += n/per*7; //进行了几天 
+	count = n/per; //进行了几周 
 	n %= per;//还剩几题
 	
 	//因为余数不可能大于除数，因此可以这样写
 	if(n<=a*5){
-		count += n/a +(n%a != 0);//布尔表达式中为正确的是1 
+		count += n/a*5 +(n%a != 0);
 	}
 	else{
 		n-=a*5;
 		count += 6+(n>b);
 	} 
+	
 	
 	
 	cout << count;
