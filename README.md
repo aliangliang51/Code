af68779f49e4e3ab699d92f705c954522c19233e
aliangliang51 <2931769856@qq.com>
2025-03-04 14:06:40 +0800

    Linux第一次提交

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # 蓝桥杯
 
 #### 介绍
+
 用于每天提交
+
+
+#### 配置官网
+
+linux https://vscode.github.net.cn/docs/cpp/config-linux
---
+++ 练习/1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+	cout<< 2*pow(9,3)+0*pow(9,2)+2*pow(9,1)+2*pow(9,0)<<endl; 
+	return 0;
+} 
