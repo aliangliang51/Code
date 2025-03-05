fc4443c04b65a3a2b5be257673392aa7f74e6b05
aliangliang51 <153522530+aliangliang51@users.noreply.github.com>
2025-03-05 09:55:26 +0800

    更新一维前缀和统计子矩阵

---
+++ 蓝桥杯/真题/第十三届/统计子矩阵.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+int main()
+{
+  ll n , m ,k,arr[100];
+  cin>>n>>m>>k;
+  for(ll i=0;i<n;i++){
+    for(ll j=0;j<m;j++){
+       cin>> arr[i][j];
+    }
+  }
+  ll sum
+  for(ll i=0;i<n;i++){
+    for(ll j=1;j<m;j++){
+        sum = arr[i][j]+arr[i][j-1];
+        
+    }}
+  return 0;
+}
